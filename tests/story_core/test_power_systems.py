8889d210d0ca44375a2cfafc37f3e619366654cf
zha <793666372@qq.com>
2026-07-26 17:33:46 +0800

    test: lock power system module boundaries

--- tests/story_core/test_power_systems.py
+++ tests/story_core/test_power_systems.py
@@ -6,6 +6,8 @@ import math
 
 import pytest
 
+from packages.story_core import power_system_spec
+from packages.story_core import power_systems as power_system_facade
 from packages.story_core.power_systems import (
     PowerSystemValidationError,
     legacy_power_summary,
@@ -16,6 +18,12 @@ from packages.story_core.power_systems import (
 
 
 CLASSES = ("战士", "法师", "游侠", "盗贼", "牧师", "召唤师")
+
+
+def test_public_facade_reexports_specification_api() -> None:
+    assert power_system_facade.PowerSystemValidationError is power_system_spec.PowerSystemValidationError
+    assert power_system_facade.normalize_power_system_spec is power_system_spec.normalize_power_system_spec
+    assert power_system_facade.validate_power_system_spec is power_system_spec.validate_power_system_spec
 
 
 def complete_spec() -> dict[str, object]:
